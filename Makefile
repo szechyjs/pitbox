06d8ba8cc0ff364b0c64b089a8ea3d509191822c
Jared Szechy <jared.szechy@gmail.com>
2013-01-02 12:50:05 -0500

    Store configuration in EEPROM

--- Makefile
+++ Makefile
@@ -2,5 +2,6 @@ ARDUINO_DIR = /usr/share/arduino
 
 BOARD_TAG = micro
 ARDUINO_PORT = /dev/ttyACM*
+ARDUINO_LIBS = EEPROM
 
 include $(ARDUINO_DIR)/Arduino.mk
--- pitbox.ino
+++ pitbox.ino
@@ -10,7 +10,9 @@
 
 *******************************************************************************/
 
-// To enable serial messages, uncomment the following line
+#include <EEPROM.h>
+
+// To enable serial debugging, uncomment the following line
 //#define VERBOSE
 
 // Pin Configuration
@@ -19,9 +21,50 @@
 #define PIN_GREEN_LEDS  10
 #define PIN_TRIGGER     9
 
-// Constants
-#define RED_ON_TIME     10 * 1000
-#define GREEN_ON_TIME   3 * 1000
+// Configuration settings
+#define CONFIG_VERSION "v1"
+#define CONFIG_START 32
+
+struct ConfigStruct {
+  unsigned int redTime, greenTime;
+  char version[4];
+} config = {
+  8, 3,
+  CONFIG_VERSION
+};
+
+void saveConfig()
+{
+  for (unsigned int t=0; t < sizeof(config); t++)
+  {
+    // Write to EEPROM
+    EEPROM.write(CONFIG_START + t, *((char*)&config + t));
+
+    // Verify the data
+    if (EEPROM.read(CONFIG_START + t) != *((char*)&config + t))
+    {
+      // Error writing to the EEPROM
+    }
+  }
+}
+
+void loadConfig()
+{
+  // Make sure there are settings,
+  // if nothing is found, use the default settings.
+  if (EEPROM.read(CONFIG_START + sizeof(config) - 1) == config.version[3] &&
+      EEPROM.read(CONFIG_START + sizeof(config) - 2) == config.version[2] &&
+      EEPROM.read(CONFIG_START + sizeof(config) - 3) == config.version[1] &&
+      EEPROM.read(CONFIG_START + sizeof(config) - 4) == config.version[0])
+  {
+    // Read config from EEPROM
+    for (unsigned int t=0; t < sizeof(config); t++)
+      *((char*)&config + t) = EEPROM.read(CONFIG_START + t);
+  } else {
+    // Settings aren't valid, overwrite with defaults
+    saveConfig();
+  }
+}
 
 void setup()
 {
@@ -29,6 +72,9 @@ void setup()
 #ifdef VERBOSE
   Serial.begin(9600);
 #endif
+
+  // Read configuration from EEPROM
+  loadConfig();
 
   // Set pin modes
   pinMode(PIN_STATUS_LED, OUTPUT);
@@ -46,14 +92,14 @@ void setup()
 void redLeds(int time)
 {
   digitalWrite(PIN_RED_LEDS, HIGH);
-  delay(time);
+  delay(time * 1000);
   digitalWrite(PIN_RED_LEDS, LOW);
 }
 
 void greenLeds(int time)
 {
   digitalWrite(PIN_GREEN_LEDS, HIGH);
-  delay(time);
+  delay(time * 1000);
   digitalWrite(PIN_GREEN_LEDS, LOW);
 }
 
@@ -63,9 +109,9 @@ void loop()
   if (digitalRead(PIN_TRIGGER) == LOW)
   {
     // Turn on red LEDs
-    redLeds(RED_ON_TIME);
+    redLeds(config.redTime);
 
     // Turn on green LEDs
-    greenLeds(GREEN_ON_TIME);
+    greenLeds(config.greenTime);
   }
 }
